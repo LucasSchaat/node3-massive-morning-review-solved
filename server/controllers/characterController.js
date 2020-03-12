50625f3801d02ae84274d4901f60a6ea66919139
Lucas Schaat <lucas.schaat@gmail.com>
2020-03-12 10:32:28 -0600

    Slight updates to characterController.js code

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -8,7 +8,7 @@ module.exports = {
     getCharacter: (req, res) => {
         const {id} = req.params
         const db = req.app.get('db')
-        db.get_character(id)
+        db.get_character(+id)
         .then(character => res.status(200).send(character))
         .catch(err => res.status(500).send(err))
     },
@@ -16,21 +16,21 @@ module.exports = {
         const character = {...req.body}
         const db = req.app.get('db')
         db.add_character(character)
-        .then(character => res.status(200).send(character))
+        .then(newCharacter => res.status(200).send(newCharacter))
         .catch(err => res.status(500).send(err))
     },
     editCharacter: (req, res) => {
         const {id} = req.params
         const {name, image} = req.body
         const db = req.app.get('db')
-        db.edit_character(id, name, image)
-        .then(character => res.status(200).send(character))
+        db.edit_character(+id, name, image)
+        .then(updatedCharacter => res.status(200).send(updatedCharacter))
         .catch(err => res.status(500).send(err))
     },
     deleteCharacter: (req, res) => {
         const {id} = req.params
         const db = req.app.get('db')
-        db.delete_character(id)
+        db.delete_character(+id)
         .then(() => res.sendStatus(200))
         .catch(err => res.status(500).send(err))
     }
